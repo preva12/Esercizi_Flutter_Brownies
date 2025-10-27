a8186b5e841873b458faa1755a6a0a640b7b71be
Prevarin Marco <marco.prevarin.24@stud.itsaltoadriatico.it>
2025-10-27 16:51:53 +0100

    invio esercizio 2 finito

---
+++ lib/main.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController _variabile =TextEditingController();
+  String _messaggio= '';
+
+  void Ciao() {
+    setState(() {
+     final messaggiofinale = _variabile.text.trim();
+     if (messaggiofinale.isEmpty){
+      _messaggio = 'inserisci il tuo nome';
+       
+     }
+      else{
+        _messaggio = 'Ciao $messaggiofinale';
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Padding(
+          padding :const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('inserisci il tuo nome:',
+                style: TextStyle(fontSize: 30),
+              ),
+              const SizedBox(height: 24),
+              TextField(
+                controller: _variabile,
+                
+
+                ),
+          
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: Ciao,
+                child: const Text('Saluta!!'),
+              ),
+              const SizedBox(height: 25),
+              Text(
+                _messaggio,
+                style: const TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+                
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _messaggio = '';
+                    _variabile.clear();
+                  });
+                },
+                child: const Text('Resetta'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
